19c360b2d2417eca983964d93398599b94c46559
Shafik-maniyar <168724956+Shafik-maniyar@users.noreply.github.com>
2024-06-05 00:34:25 +0530

    Add files via upload

---
+++ temp.php
@@ -0,0 +1,12 @@
+<?php
+
+$degree=$_REQUEST['degree'];
+$farenheit=$_REQUEST['farenheit'];
+$result=$_REQUEST['result'];
+$convert=$_REQUEST['convert'];
+
+$result=($degree*9/5)+32
+
+echo"<br>$result";
+
+?>
